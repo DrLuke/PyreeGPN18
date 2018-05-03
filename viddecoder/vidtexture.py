b3d51ce0276f3354e8618788e6a437f48222a71a
DrLuke <Lukas@Jackowski.de>
2018-05-04 01:02:50 +0200

    Add video decoding to opengl texture

---
+++ viddecoder/vidtexture.py
@@ -0,0 +1,48 @@
+from pyreeEngine.textures import Texture
+from viddecoder.viddecoder import VidDecoder
+
+from pathlib import Path
+
+from OpenGL.GL import *
+
+import numpy as np
+
+class VideoTexture(Texture):
+    def __init__(self, path: Path):
+        super(VideoTexture, self).__init__()
+
+        self.decoder = VidDecoder(path)
+
+        if self.textures is not None:
+            glDeleteTextures(self.textures)  # Clean up old texture
+        self.textures = [glGenTextures(1)]
+
+    def fetchNext(self):
+        nextFrame = self.decoder.loadNextFrame()
+        if nextFrame is not None:
+            """imdata = []
+
+            for j in range(self.decoder.height):
+                for i in range(self.decoder.width):
+                    pix = i + j*self.decoder.width
+                    imdata += [nextFrame[0][pix], nextFrame[0][pix], nextFrame[0][pix]]
+
+            imdata = np.array(imdata, np.uint8)
+            #imdata = np.array(imdata, 'B')"""
+
+            new_array = self.decoder.frameToNumpyArray(nextFrame)
+
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+            glBindTexture(GL_TEXTURE_2D, self.textures[0])
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, self.decoder.width, self.decoder.height, 0, GL_RGB, GL_UNSIGNED_BYTE,
+                         new_array)
+            #print(imdata)
+            glGenerateMipmap(GL_TEXTURE_2D)
+
+
+    def getTexture(self):
+        return self.textures[0]
